5ab113e27bdb908fb76d4bbc77ec77eab349b474
Szabo Henrique <szaboprofissional@gmail.com>
2025-02-24 22:20:10 -0300

    done

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,3 +1,5 @@
+'use client'
+import { useState } from 'react'
 import Footer from '@/components/Footer'
 import Logo from '@/components/Logo'
 import Menu from '@/components/Menu'
@@ -7,6 +9,14 @@ import React from 'react'
 import CardUser from '@/components/CardUser'
 
 export default function Home() {
+  const [people, setPeople] = useState([])
+  const [input1, setInput1] = useState('')
+  const [input2, setInput2] = useState('')
+
+  const handleAddUser = (name, email) => {
+   setPeople([...people, {name, email}])
+  }
+
   return (
     <main className='flex flex-col justify-between border border-white mx-40 p-10'>
       <div className="flex justify-between">
@@ -16,8 +26,10 @@ export default function Home() {
       <div className="flex gap-4 items-center p-6 bg-neutral-800">
         <Sidebar/>
         <Texto title="About">
-          <CardUser userName="Szabo Henrique" userEmail="szaboprofissional@gmail.com" userImage="https://github.com/Szaboltz.png"/>
-          <CardUser userName="Emanuel Cardoso" userEmail="emanuel@gmail.com" userImage="https://github.com/EmanuelCAC.png"/>
+          {people.map((person, i) => <CardUser key={i} userName={person.name} userEmail={person.email}/>)}
+          <input type="text" value={input1} onChange={(event) => {setInput1(event.target.value)}} className='px-7 py-2 rounded-lg bg-neutral-800' placeholder='Digite seu nome'/>
+          <input type="text" value={input2} onChange={(event) => {setInput2(event.target.value)}} className='px-7 py-2 rounded-lg bg-neutral-800' placeholder='Digite seu email'/>
+          <button className='px-7 py-2 bg-red-800' onClick={() => {handleAddUser(input1, input2)}}>Cadastrar</button>
         </Texto>
       </div>
       <Footer/>
